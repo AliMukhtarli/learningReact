1dcfd9352006f69c661ff78957b6a54e242fcb22
Əli Muxtarlı <alimukhtarli085@gmail.com>
2026-05-10 16:13:54 +0400

    Button click events learning proses

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,21 @@
 
 function Button(){
-    const handleClick = () => console.log("Don't click me!😠")
+   //const handlecClick2 = (name) => console.log(`${name} stop clicking me!`) 
+
+    let count = 0;
+     const handleClick = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} times!`)
+        }else{
+            console.log(`${name} stop cilcking me!!!`)
+        }
+     }
+
+
+
     return(
-        <button className="button" onClick={handleClick}>Click me!😁</button>
+        <button className="button" onClick={ () => handleClick("Ali") }>Click me!😁</button>
     );
 }
 
