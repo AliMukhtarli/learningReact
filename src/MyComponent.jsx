92a6308a27ae9bd514ca8c21e35fe418d4b2733a
Əli Muxtarlı <alimukhtarli085@gmail.com>
2026-07-03 13:42:11 +0400

    Textarea is done

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -7,6 +7,7 @@ function MyComponent() {
 
     const [name, setName] = useState('Guest');
     const [age, setAge] = useState(18);
+    const [comment, setComment] = useState("");
     function handleChange(event) {
         setName(event.target.value);
     }
@@ -14,12 +15,21 @@ function MyComponent() {
     function handleAgeChange(event){
         setAge(event.target.value);
     }
+
+    function handleCommentChange(event){
+        setComment(event.target.value);
+    }
  
     return (<div>
             <input value={name} onChange={handleChange} />
             <p>Name: {name}</p>
+
             <input type="number" value={age} onChange={handleAgeChange} />
             <p>Age: {age}</p>
+
+            <textarea value={comment} onChange={handleCommentChange}  placeholder="Enter your comment"/>
+            <p>Comment: {comment}</p>
+            
             </div>)
 
 
