767cad4f721ada20cdc41e7e30978955fca86f9f
Əli Muxtarlı <alimukhtarli085@gmail.com>
2026-07-03 13:32:10 +0400

    practicing on numbers

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -6,14 +6,20 @@ import Recat, {useState} from 'react';
 function MyComponent() {
 
     const [name, setName] = useState('Guest');
-
+    const [age, setAge] = useState(18);
     function handleChange(event) {
         setName(event.target.value);
+    }
+
+    function handleAgeChange(event){
+        setAge(event.target.value);
     }
  
     return (<div>
             <input value={name} onChange={handleChange} />
             <p>Name: {name}</p>
+            <input type="number" value={age} onChange={handleAgeChange} />
+            <p>Age: {age}</p>
             </div>)
 
 
